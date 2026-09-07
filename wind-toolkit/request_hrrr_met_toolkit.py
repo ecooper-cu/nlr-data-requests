144f95edd79a97231eb95047a8796f6152d21e45
ecooper-cu <emco4286@colorado.edu>
2026-09-07 15:47:23 -0600

    wind

--- wind-toolkit/request_hrrr_met_toolkit.py
+++ wind-toolkit/request_hrrr_met_toolkit.py
@@ -18,10 +18,11 @@ for plant in network_wind.index:
     plant_to_coords[plant] = (lat, lon)
 
 years = [2015, 2024, 2025]
+attributes = ['windspeed_100m', 'pressure_100m', 'temperature_100m']
 
 data_directory = '/nrel/wtk/hrrr_met_toolkit/v1.0.0/'
 
-plant_to_year = {p:{} for p in plant_to_coords.keys()}
+plant_to_year_to_data = {p:{year: {} for year in years} for p in plant_to_coords.keys()}
 
 for year in years:
     print(f"Pulling data for {year}...")
@@ -30,6 +31,18 @@ for year in years:
         time_index = f.time_index
         for plant, location in plant_to_coords.items():
             print(f"Pulling data for {plant}")
-            data = f.get_lat_lon_df('windspeed_100m', location)
-            data.set_index(time_index, inplace=True)
-            data.to_csv(f"/home/emco4286/data/wind/raw/{year}/{plant}.csv")
+            for attr in attributes:
+                data = f.get_lat_lon_df(attr, location)
+                data.set_index(time_index, inplace=True)
+                plant_to_year_to_data[plant][year][attr] = data
+
+plant_to_data = {}
+for plant in plant_to_coords.keys():
+    data_list = []
+    for year in years:
+        data = pd.concat(plant_to_year_to_data[plant][year].values(), axis=1)
+        data.columns = attributes
+        data.set_index(plant_to_year_to_data[plant][year][attributes[0]].index, inplace=True)
+        print(f"Saving data for {plant}...")
+        print(data.head())
+        data.to_csv(f"/home/emco4286/data/wind/raw/{year}/{plant}.csv")
--- wind-toolkit/request_wtk_conus.py
+++ wind-toolkit/request_wtk_conus.py
@@ -18,10 +18,11 @@ for plant in network_wind.index:
     plant_to_coords[plant] = (lat, lon)
 
 years = [2014]
+attributes = ['windspeed_100m', 'pressure_100m', 'temperature_100m']
 
 data_directory = '/nrel/wtk/conus/'
 
-plant_to_year = {p:{} for p in plant_to_coords.keys()}
+plant_to_year_to_data = {p:{year: {} for year in years} for p in plant_to_coords.keys()}
 
 for year in years:
     print(f"Pulling data for {year}...")
@@ -32,6 +33,20 @@ for year in years:
         time_index = f.time_index
         for plant, location in plant_to_coords.items():
             print(f"Pulling data for {plant}")
-            data = f.get_lat_lon_df('windspeed_100m', location)
-            data.set_index(time_index, inplace=True)
-            data.to_csv(f"/home/emco4286/data/wind/raw/{year}/{plant}.csv")
+            for attr in attributes:
+                data = f.get_lat_lon_df(attr, location)
+                data.set_index(time_index, inplace=True)
+                plant_to_year_to_data[plant][year][attr] = data
+
+plant_to_data = {}
+for plant in plant_to_coords.keys():
+    data_list = []
+    for year in years:
+        data = pd.concat(plant_to_year_to_data[plant][year].values(), axis=1)
+        data.columns = attributes
+        data.set_index(plant_to_year_to_data[plant][year][attributes[0]].index, inplace=True)
+        print(data.head())
+        data_list.append(data)
+    data = pd.concat(data_list, axis=0)
+    data.columns = attributes
+    data.to_csv(f"/home/emco4286/data/wind/raw/{year}/{plant}.csv")
